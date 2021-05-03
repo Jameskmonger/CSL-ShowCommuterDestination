d03d086fd1553f4d600f0cc7c89ab143c6b325a5
James Monger <jameskmonger@hotmail.co.uk>
2021-05-03 16:52:10 +0100

    Add previous/next buttons
    Closes #2

--- Content/StopDestinationInfoPanel.cs
+++ Content/StopDestinationInfoPanel.cs
@@ -49,20 +49,37 @@ namespace CSLShowCommuterDestination
             }
 
             this.stopId = stopId;
-            this.transportLineId = Singleton<NetManager>.instance.m_nodes.m_buffer[this.stopId].m_transportLine;
+            var node = Singleton<NetManager>.instance.m_nodes.m_buffer[this.stopId];
+            this.transportLineId = node.m_transportLine;
 
             Debug.Log("Valid instance ID for StopDestinationInfoPanel");
             WorldInfoPanel.HideAllWorldInfoPanels();
+            this.m_LineNameLabel.text = Singleton<TransportManager>.instance.GetLineName(this.transportLineId) + " destinations";
             this.m_StopNameLabel.text = "Stop #" + this.GetStopIndex();
-            this.m_LineNameLabel.text = "Line: " + Singleton<TransportManager>.instance.GetLineName(this.transportLineId);
 
             int passengerCount = Singleton<TransportManager>.instance.m_lines.m_buffer[this.transportLineId].CalculatePassengerCount(this.stopId);
             this.m_PassengerCountLabel.text = "Waiting passengers: " + passengerCount;
 
             this.m_BuildingPopularities = this.GetPositionPopularities();
 
+            ToolsModifierControl.cameraController.SetTarget(instanceId, node.m_position, false);
+
             this.Show();
             this.LateUpdate();
+        }
+
+        public void MoveToPrevStop()
+        {
+            var prevStop = TransportLine.GetPrevStop(this.stopId);
+
+            this.Show(prevStop);
+        }
+
+        public void MoveToNextStop()
+        {
+            var nextStop = TransportLine.GetNextStop(this.stopId);
+
+            this.Show(nextStop);
         }
 
         private IEnumerable<KeyValuePair<ushort, int>> GetPositionPopularities()
@@ -126,7 +143,9 @@ namespace CSLShowCommuterDestination
             while (stop != 0)
             {
                 if (this.stopId == (int)stop)
+                {
                     return index;
+                }
                 ++index;
                 stop = TransportLine.GetNextStop(stop);
                 if (index >= 32768)
@@ -153,25 +172,107 @@ namespace CSLShowCommuterDestination
             this.canFocus = true;
             this.isInteractive = true;
             this.anchor = UIAnchorStyle.None;
-            this.pivot = UIPivotPoint.BottomRight;
-            this.width = 380f;
-            this.height = 380f;
-            this.backgroundSprite = "InfoBubbleVehicle";
+            this.pivot = UIPivotPoint.MiddleCenter;
+            this.transformPosition = new Vector3(1.5f, 0f);
+            this.width = 250f;
+            this.height = 150f;
+            this.backgroundSprite = "MenuPanel";
 
-            this.m_LineNameLabel = this.AddUIComponent<UILabel>();
+            this.padding = new RectOffset(10, 10, 5, 5);
+
+            UILabel title = this.AddUIComponent<UILabel>();
+            title.name = "TitleLabel";
+            title.text = "Commuter Destinations";
+            title.relativePosition = new Vector3(10.0f, 10.0f);
+            title.textColor = new Color32(231, 220, 161, 255);
+
+            UIPanel container = this.AddUIComponent<UIPanel>();
+            container.name = "Container";
+            container.width = this.width;
+            container.height = 100.0f;
+            container.autoLayout = true;
+            container.autoLayoutDirection = LayoutDirection.Vertical;
+            container.autoLayoutPadding = new RectOffset(0, 0, 5, 5);
+            container.autoLayoutStart = LayoutStart.TopLeft;
+            container.relativePosition = new Vector3(0.0f, 40.0f);
+            container.padding = new RectOffset(10, 10, 5, 5);
+
+            this.m_LineNameLabel = container.AddUIComponent<UILabel>();
             this.m_LineNameLabel.name = "LineNameLabel";
             this.m_LineNameLabel.text = "Line Name";
-            this.m_LineNameLabel.relativePosition = new Vector3(0.0f, 0.0f);
+            this.m_LineNameLabel.textScale = 0.8f;
+            this.m_LineNameLabel.relativePosition = new Vector3(0.0f, 20.0f);
 
-            this.m_StopNameLabel = this.AddUIComponent<UILabel>();
+            this.m_StopNameLabel = container.AddUIComponent<UILabel>();
             this.m_StopNameLabel.name = "StopNameLabel";
             this.m_StopNameLabel.text = "Stop Name";
-            this.m_StopNameLabel.relativePosition = new Vector3(0.0f, 20.0f);
+            this.m_StopNameLabel.textScale = 0.8f;
+            this.m_StopNameLabel.relativePosition = new Vector3(0.0f, 40.0f);
 
-            this.m_PassengerCountLabel = this.AddUIComponent<UILabel>();
+            UIPanel stopNavigation = container.AddUIComponent<UIPanel>();
+            stopNavigation.name = "StopNavigation";
+            stopNavigation.width = container.width;
+            stopNavigation.height = 30.0f;
+            stopNavigation.autoLayout = true;
+            stopNavigation.autoLayoutDirection = LayoutDirection.Horizontal;
+            stopNavigation.autoLayoutPadding = new RectOffset(0, 0, 0, 0);
+            stopNavigation.autoLayoutStart = LayoutStart.TopLeft;
+            stopNavigation.padding = new RectOffset(0, 0, 0, 0);
+            stopNavigation.relativePosition = new Vector3(0.0f, 60.0f);
+
+            UIButton previousStop = this.CreateButton(stopNavigation);
+            previousStop.name = "PreviousStop";
+            previousStop.textPadding = new RectOffset(10, 10, 4, 0);
+            previousStop.text = "Previous";
+            previousStop.tooltip = "Navigate to the previous stop";
+            previousStop.textScale = 0.75f;
+            previousStop.size = new Vector2(110f, 30f);
+            previousStop.wordWrap = true;
+            previousStop.eventClick += new MouseEventHandler(this.OnPrevStopButtonClick);
+
+            UIButton nextStop = this.CreateButton(stopNavigation);
+            nextStop.name = "NextStop";
+            nextStop.textPadding = new RectOffset(10, 10, 4, 0);
+            nextStop.text = "Next";
+            nextStop.tooltip = "Navigate to the next stop";
+            nextStop.textScale = 0.75f;
+            nextStop.size = new Vector2(110f, 30f);
+            nextStop.wordWrap = true;
+            nextStop.eventClick += new MouseEventHandler(this.OnNextStopButtonClick);
+
+            this.m_PassengerCountLabel = container.AddUIComponent<UILabel>();
             this.m_PassengerCountLabel.name = "PassengerCountLabel";
             this.m_PassengerCountLabel.text = "Passenger Count";
-            this.m_PassengerCountLabel.relativePosition = new Vector3(0.0f, 40.0f);
+            this.m_PassengerCountLabel.textScale = 0.8f;
+            this.m_PassengerCountLabel.relativePosition = new Vector3(0.0f, 90.0f);
+        }
+
+        private void OnPrevStopButtonClick(UIComponent component, UIMouseEventParameter eventParam)
+        {
+            this.MoveToPrevStop();
+        }
+
+        private void OnNextStopButtonClick(UIComponent component, UIMouseEventParameter eventParam)
+        {
+            this.MoveToNextStop();
+        }
+
+        private UIButton CreateButton(UIComponent parent)
+        {
+            UIButton uiButton = parent.AddUIComponent<UIButton>();
+            uiButton.font = GameObject.Find("(Library) PublicTransportInfoViewPanel").GetComponent<PublicTransportInfoViewPanel>().Find<UILabel>("Label").font;
+            uiButton.textPadding = new RectOffset(0, 0, 4, 0);
+            uiButton.normalBgSprite = "ButtonMenu";
+            uiButton.disabledBgSprite = "ButtonMenuDisabled";
+            uiButton.hoveredBgSprite = "ButtonMenuHovered";
+            uiButton.focusedBgSprite = "ButtonMenu";
+            uiButton.pressedBgSprite = "ButtonMenuPressed";
+            uiButton.textColor = new Color32(255, 255, 255, 255);
+            uiButton.disabledTextColor = new Color32(7, 7, 7, 255); ;
+            uiButton.hoveredTextColor = new Color32(255, 255, 255, 255);
+            uiButton.focusedTextColor = new Color32(255, 255, 255, 255);
+            uiButton.pressedTextColor = new Color32(30, 30, 44, 255);
+            return uiButton;
         }
     }
 }
