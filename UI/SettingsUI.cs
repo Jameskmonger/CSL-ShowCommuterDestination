93c0b83dc3abd9ea9e5bc865f3900a6c9c34b5cc
James Monger <jameskmonger@hotmail.co.uk>
2022-12-30 22:27:48 +0000

    refactor: create SettingsUI class

---
+++ UI/SettingsUI.cs
@@ -0,0 +1,23 @@
+﻿using ColossalFramework.UI;
+using CSLShowCommuterDestination.Game.Integrations;
+
+namespace CSLShowCommuterDestination.UI
+{
+    public class SettingsUI
+    {
+        public static void BuildPanel(UIHelper helper)
+        {
+            BuildIntegrationsPanel(helper);
+        }
+
+        private static void BuildIntegrationsPanel(UIHelper helper)
+        {
+            var integrationsGroup = helper.AddGroup("Integrations") as UIHelper;
+            var integrationsPanel = integrationsGroup.self as UIPanel;
+
+            var ipt2Label = integrationsPanel.AddUIComponent<UILabel>();
+            ipt2Label.name = "integration_ipt2";
+            ipt2Label.text = "Improved Public Transport 2: " + (ModIntegrations.IsIPT2Enabled() ? "Detected" : "Not detected");
+        }
+    }
+}
