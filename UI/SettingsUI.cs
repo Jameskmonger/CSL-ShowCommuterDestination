d3f6774034882ad0d77dd0a965cb7fd818f69b48
James Monger <jameskmonger@hotmail.co.uk>
2022-12-31 13:08:17 +0000

    docs for settings

--- UI/SettingsUI.cs
+++ UI/SettingsUI.cs
@@ -3,14 +3,25 @@ using CSLShowCommuterDestination.Game.Integrations;
 
 namespace CSLShowCommuterDestination.UI
 {
+    /// <summary>
+    /// The settings panel for the mod.
+    /// </summary>
     public class SettingsUI
     {
+        /// <summary>
+        /// Builds the settings panel with the given UIHelper.
+        /// </summary>
+        /// <param name="helper">the UIHelper to build with</param>
         public static void BuildPanel(UIHelper helper)
         {
-            BuildIntegrationsPanel(helper);
+            BuildIntegrationsSettings(helper);
         }
 
-        private static void BuildIntegrationsPanel(UIHelper helper)
+        /// <summary>
+        /// Builds the integrations settings with the given UIHelper.
+        /// </summary>
+        /// <param name="helper">the UIHelper to build with</param>
+        private static void BuildIntegrationsSettings(UIHelper helper)
         {
             var integrationsGroup = helper.AddGroup("Integrations") as UIHelper;
             var integrationsPanel = integrationsGroup.self as UIPanel;
