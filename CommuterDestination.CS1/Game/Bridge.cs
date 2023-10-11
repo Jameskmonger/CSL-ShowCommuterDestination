23bd935830b5491237d01354756430023bf12b88
James Monger <jameskmonger@hotmail.co.uk>
2023-10-11 23:08:04 +0100

    refactor: split graph generator intro bridge / core

--- CommuterDestination.CS1/Game/Bridge.cs
+++ CommuterDestination.CS1/Game/Bridge.cs
@@ -1,5 +1,7 @@
 ﻿using ColossalFramework;
 using CommuterDestination.Core.Bridge;
+using CommuterDestination.Core.Citizen;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace CommuterDestination.CS1.Game
@@ -271,6 +273,75 @@ namespace CommuterDestination.CS1.Game
         public Vector3 GetBuildingPosition(ushort buildingId)
         {
             return Singleton<BuildingManager>.instance.m_buildings.m_buffer[buildingId].m_position;
+        }
+
+        /// <remarks>This logic is taken from LoadPassengers in the game logic (e.g. in BusAI)</remarks>
+        public IEnumerable<CitizenDestination> GetCitizenDestinations(ushort stopId)
+        {
+            List<CitizenDestination> citizenDestinations = new List<CitizenDestination>();
+
+            var transitRange = GameBridge.Instance.GetStopRange(stopId);
+
+            Vector3 stopPosition = GameBridge.Instance.GetStopPosition(stopId);
+
+            int LOWER_BOUND_X = Mathf.Max((int)((stopPosition.x - transitRange) / 8.0 + 1080.0), 0);
+            int UPPER_BOUND_X = Mathf.Min((int)((stopPosition.x + transitRange) / 8.0 + 1080.0), 2159);
+            int LOWER_BOUND_Z = Mathf.Max((int)((stopPosition.z - transitRange) / 8.0 + 1080.0), 0);
+            int UPPER_BOUND_Z = Mathf.Min((int)((stopPosition.z + transitRange) / 8.0 + 1080.0), 2159);
+
+            for (int z = LOWER_BOUND_Z; z <= UPPER_BOUND_Z; ++z)
+            {
+                for (int x = LOWER_BOUND_X; x <= UPPER_BOUND_X; ++x)
+                {
+                    ushort citizenInstanceId = Singleton<CitizenManager>.instance.m_citizenGrid[z * 2160 + x];
+                    while (citizenInstanceId != (ushort)0)
+                    {
+                        CitizenInstance citizen = Singleton<CitizenManager>.instance.m_instances.m_buffer[(int)citizenInstanceId];
+                        ushort nextGridInstance = citizen.m_nextGridInstance;
+
+                        if (IsCitizenAtStop(ref citizen, citizenInstanceId, stopId, transitRange))
+                        {
+                            citizenDestinations.Add(new CitizenDestination
+                            {
+                                stopId = GameBridge.Instance.GetDestinationStopId(stopId, citizenInstanceId),
+                                buildingId = citizen.m_targetBuilding
+                            });
+                        }
+
+                        citizenInstanceId = nextGridInstance;
+                    }
+                }
+            }
+
+            return citizenDestinations;
+        }
+
+        /// <summary>
+        /// Check if a given citizen is waiting at a given stop.
+        /// </summary>
+        /// <param name="citizen">the citizen to check</param>
+        /// <param name="citizenInstanceId">the citizen's instance ID</param>
+        /// <param name="stopId">the stop to check</param>
+        /// <param name="stopRange">the range of the stop</param>
+        /// <returns>`true` if the citizen is waiting at the stop, false otherwise</returns>
+        private static bool IsCitizenAtStop(ref CitizenInstance citizen, ushort citizenInstanceId, ushort stopId, float stopRange)
+        {
+            var citizenIsAtStop = GameBridge.Instance.IsCitizenInRangeOfStop(citizenInstanceId, stopId, (double)stopRange);
+
+            // If the citizen is not within range, no need to check further
+            if (!citizenIsAtStop)
+            {
+                return false;
+            }
+
+            ushort nextStop = GameBridge.Instance.GetNextStop(stopId);
+            Vector3 stopPosition = GameBridge.Instance.GetStopPosition(stopId);
+            Vector3 nextStopPosition = GameBridge.Instance.GetStopPosition(nextStop);
+
+            return (
+                (citizen.m_flags & CitizenInstance.Flags.WaitingTransport) != CitizenInstance.Flags.None
+                && citizen.Info.m_citizenAI.TransportArriveAtSource(citizenInstanceId, ref citizen, stopPosition, nextStopPosition)
+            );
         }
     }
 }
